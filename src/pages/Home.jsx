d3f255d6280c6ff4aa137da4868d6ebf04e1edb5
DESKTOP-189F2TD\jux33 <julien.pardoel@gmail.com>
2021-05-12 10:28:17 +0200

    base du projet

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="home">
+            hello from home
+        </div>
+    )
+}
+
+export default Home
---
+++ src/pages/Profil.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profil = () => {
+    return (
+        <div className="profil">
+            hello from profil
+        </div>
+    )
+}
+
+export default Profil
